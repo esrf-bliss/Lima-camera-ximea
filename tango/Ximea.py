23638c0699b229fd6acb9350e5aea1cbfd813629
daneos <daneos@daneos.com>
2020-12-09 23:25:25 +0100

    add python tango code (based on Basler plugin)

--- tango/Ximea.py
+++ tango/Ximea.py
@@ -19,11 +19,119 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 ############################################################################
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # The Ximea camera plugin TANGO interface
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 import PyTango
 
 from Lima import Core
 from Lima import Ximea as Xi
+from Lima.Server import AttrHelper
+
+
+class Ximea(PyTango.Device_4Impl):
+	Core.DEB_CLASS(Core.DebModApplication, 'LimaCCDs')
+
+	# ------------------------------------------------------------------
+	#    Device constructor
+	# ------------------------------------------------------------------
+	def __init__(self, *args) :
+		PyTango.Device_4Impl.__init__(self, *args)
+		self.init_device()
+
+	# ------------------------------------------------------------------
+	#    Device destructor
+	# ------------------------------------------------------------------
+	def delete_device(self):
+		pass
+
+	# ------------------------------------------------------------------
+	#    Device initialization
+	# ------------------------------------------------------------------
+	@Core.DEB_MEMBER_FUNCT
+	def init_device(self):
+		self.set_state(PyTango.DevState.ON)
+		self.get_device_properties(self.get_device_class())
+
+	# ------------------------------------------------------------------
+	#    getAttrStringValueList command:
+	#
+	#    Description: return a list of authorized values if any
+	#    argout: DevVarStringArray
+	# ------------------------------------------------------------------
+	@Core.DEB_MEMBER_FUNCT
+	def getAttrStringValueList(self, attr_name):
+		# use AttrHelper
+		return AttrHelper.get_attr_string_value_list(self, attr_name)
+
+	# ------------------------------------------------------------------
+	#
+	#    Ximea read/write attribute methods
+	#
+	# ------------------------------------------------------------------
+	def __getattr__(self, name):
+		# use AttrHelper
+		return AttrHelper.get_attr_4u(self, name, _XimeaCam)
+
+
+# ------------------------------------------------------------------
+#
+#    BaslerClass class definition
+#
+# ------------------------------------------------------------------
+class XimeaClass(PyTango.DeviceClass):
+
+	class_property_list = {}
+
+	device_property_list = {
+		'camera_id': [
+			PyTango.DevString,
+			"Camera ID",
+			None
+		],
+	}
+
+	cmd_list = {
+		'getAttrStringValueList': [
+			[PyTango.DevString, "Attribute name"],
+			[PyTango.DevVarStringArray, "Authorized String value list"]
+		],
+	}
+
+	attr_list = {}
+
+	def __init__(self, name):
+		PyTango.DeviceClass.__init__(self, name)
+		self.set_type(name)
+
+
+# ----------------------------------------------------------------------------
+# Plugins
+# ----------------------------------------------------------------------------
+_XimeaCam = None
+_XimeaInterface = None
+
+
+def get_control(**keys):
+	global _XimeaCam
+	global _XimeaInterface
+
+	if 'camera_id' in keys:
+		camera_id = keys['camera_id']
+	else:
+		# TODO: probably want to throw an exception here
+		pass
+
+	print("Ximea camera_id:", camera_id)
+
+	# all properties are passed as string from LimaCCDs device get_control helper
+	# so need to be converted to correct type
+	if _XimeaCam is None:
+		_XimeaCam = Xi.Camera(int(camera_id))
+		_XimeaInterface = Xi.Interface(_XimeaCam)
+	return Core.CtControl(_XimeaInterface)
+
+
+def get_tango_specific_class_n_device():
+	return XimeaClass, Ximea
